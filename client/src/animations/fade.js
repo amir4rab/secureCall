062e07ee488c74eb57843835d932c5073923ee8b
Amir Arab <amir4rab@gmail.com>
2021-12-01 10:06:01 +0330

    adde helper function

---
+++ client/src/animations/fade.js
@@ -0,0 +1,113 @@
+const duration = .3;
+const transitionDistance = '1rem'
+
+export const fadeUp = {
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: .3
+    }
+  },
+  hidden: {
+    y: `-${transitionDistance}`,
+    opacity: 0,
+    transition: {
+      duration
+    }
+  } 
+};
+
+export const fadeDown = {
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: .3
+    }
+  },
+  hidden: {
+    y: `${transitionDistance}`,
+    opacity: 0,
+    transition: {
+      duration
+    }
+  } 
+};
+
+export const fadeLeft = {
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: .3
+    }
+  },
+  hidden: {
+    x: `-${transitionDistance}`,
+    opacity: 0,
+    transition: {
+      duration
+    }
+  } 
+};
+
+export const fadeRight = {
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: .3
+    }
+  },
+  hidden: {
+    x: `${transitionDistance}`,
+    opacity: 0,
+    transition: {
+      duration
+    }
+  } 
+};
+
+export const generateFade = ({ from, distance, duration, delay }) => {
+  const visible = {
+    opacity: 1,
+    transition: {
+      duration,
+      delay
+    }
+  };
+  const hidden = {
+    opacity: 0,
+    transition: {
+      duration,
+      delay
+    }
+  };
+  switch(from) {
+    case 'down': {
+      visible['y'] = 0;
+      hidden['y'] = `${distance}`;
+      break;
+    }
+    case 'up': {
+      visible['y'] = 0;
+      hidden['y'] = `-${distance}`;
+      break;
+    }
+    case 'right': {
+      visible['x'] = 0;
+      hidden['x'] = `${distance}`;
+      break;
+    }
+    case 'left': {
+      visible['x'] = 0;
+      hidden['x'] = `-${distance}`;
+      break;
+    }
+  }
+  return ({
+    visible,
+    hidden
+  });
+}
