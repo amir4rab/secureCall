4dac633d1e2d0e762b24c8d50b9ea204d506a561
Amir Arab <amir4rab@gmail.com>
2021-12-05 09:39:08 +0330

    added helper function

---
+++ server/utils/getUserData.js
@@ -0,0 +1,19 @@
+const getUserData = async ( email ,client, includeSensitiveData = false ) => {
+  await client.connect();
+  const database = client.db('secureCall');
+  const users = database.collection('users');
+  const userDBData = await users.findOne({ email });
+  await client.close();
+  if ( includeSensitiveData ) {
+    return userDBData;
+  } else {
+    return {
+      requests: userDBData.requests,
+      contacts: userDBData.contacts,
+      email: userDBData.email,
+      name: userDBData.name,
+    }
+  }
+};
+
+module.exports = getUserData;
