75e1c8bf71657c2369dce09c68767b2d6262d639
Amir Arab <amir4rab@gmail.com>
2021-12-14 10:41:07 +0330

    logoButton improvments

--- client/src/components/logoButton/logoButton.jsx
+++ client/src/components/logoButton/logoButton.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import classes from './logoButton.module.scss';
 
 
-function LogoButton({ children, name, onClick }) {
+function LogoButton({ children, name, onClick, disabled }) {
   return (
-    <button className={ classes.logoButton } onClick={ onClick }>
+    <button 
+      className={ classes.logoButton } 
+      onClick={ onClick } 
+      disabled={ disabled }
+    >
       <div className={ classes.imageWrapper }>
         { children }
       </div>
