9cacd8bfa453e9a63f4fcf550bc560535b6e84f3
Amir Arab <amir4rab@gmail.com>
2021-12-15 10:30:45 +0330

    updates to panel

--- client/src/components/panel/panel.jsx
+++ client/src/components/panel/panel.jsx
@@ -1,57 +1,33 @@
 import { useContext, useEffect, useState } from 'react';
-import { useSession } from 'next-auth/react';
+import { LayoutGroup } from 'framer-motion';
 
 import Sidebar from '../sidebar/sidebar';
-import classes from './panel.module.scss';
 import ContactDisplay from '../contactDisplay/contactDisplay';
-import { motion, LayoutGroup } from 'framer-motion';
-import { generateFade } from '../../animations/fade';
-import { SocketsContext } from '../../providers/socketsProvider/socketsProvider';
-import AddContactsPopup from '../popups/addContactPopup/addContactsPopup';
-import RequestsPopup from '../popups/requestsPopup/requestsPopup';
 
-const fadeAnimation = generateFade({ 
-  from:'up', 
-  distance: '1rem',
-  duration: '.3',
-  delay:'.15' 
-});
+import { SocketsContext } from '../../providers/socketsProvider/socketsProvider';
+
+import classes from './panel.module.scss';
 
 function Panel() {
-  const [ addContactPopup, setAddContactPopup ] = useState(false);
-  const [ showRequestsPopup, setShowRequestsPopup ] = useState(false);
   const {
     isConnected
   } = useContext(SocketsContext);
 
 
   return (
-    <LayoutGroup>
-      <AddContactsPopup 
-        displayState={ addContactPopup } 
-        setDisplayState={ setAddContactPopup } 
-      />
-      <RequestsPopup
-        displayState={ showRequestsPopup } 
-        setDisplayState={ setShowRequestsPopup } 
-      />
+    <>
       {
         isConnected ?
-        <motion.div 
-          className={ classes.panel }
-          variants={ fadeAnimation }
-          animate='visible'
-          initial='hidden'
-          hidden='hidden'
-        >
+        <div className={ classes.panel } >
           <div className={ classes.inner } >
-            <Sidebar setAddContactPopup={ setAddContactPopup } setShowRequestsPopup={ setShowRequestsPopup }  />
-            <ContactDisplay />
+
+              <Sidebar />
+              <ContactDisplay />
           </div>
-        </motion.div>
+        </div>
         : null
       }
-    </LayoutGroup>
+    </>
   )
 }
 
