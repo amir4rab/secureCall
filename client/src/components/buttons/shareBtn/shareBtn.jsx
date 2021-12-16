384ccb76f2f637a93165f0612a9642fe7e28a1b5
Amir Arab <amir4rab@gmail.com>
2021-12-16 19:21:09 +0330

    added shareButton

---
+++ client/src/components/buttons/shareBtn/shareBtn.jsx
@@ -0,0 +1,49 @@
+import useTranslation from 'next-translate/useTranslation';
+import { useEffect, useState } from 'react';
+import { IoShare } from 'react-icons/io5';
+
+import classes from './shareBtn.module.scss';
+
+function ShareBtn({ copyValue }) {
+  const { t } = useTranslation('common');
+  const [ isCopied, setIsCopied ] = useState(false);
+
+  useEffect( _ => {
+    let timeout;
+    if ( isCopied ) {
+      timeout = setTimeout( _ => {
+        setIsCopied(false)
+      }, 5000 );
+    }
+    return () => {
+      clearTimeout(timeout);
+    }
+  }, [ isCopied ]);
+
+  const eventHandler = () => {
+    try {
+      navigator.clipboard.writeText(copyValue)
+      setIsCopied(true);
+    } catch {
+      console.warn('Failed to copy text into clipboard!')
+    }
+  }
+
+  return (
+    <button type="button" onClick={ eventHandler } className={ classes.shareBtn }>
+      <IoShare />
+        {
+          isCopied ?
+          <p>
+            {t('copied')}
+          </p> 
+          :
+          <p>
+            {t('copy')}
+          </p>
+        }
+    </button>
+  )
+}
+
+export default  ShareBtn;
