275057f7fed21103de6d60c55886d534d369e4cc
Amir Arab <amir4rab@gmail.com>
2021-12-22 18:15:25 +0330

    sidebar updated

--- client/src/components/sidebar/sidebar.jsx
+++ client/src/components/sidebar/sidebar.jsx
@@ -1,33 +1,57 @@
-import React from 'react'
-import Contacts from '../contacts/contacts';
-import classes from './sidebar.module.scss';
-import useTranslation from 'next-translate/useTranslation'
-import PopupButtons from './popupButtons';
+import { useContext, useState } from 'react';
+import { useRouter } from 'next/router';
 
-function Sidebar({ setAddContactPopup, setShowRequestsPopup }) {
+import useTranslation from 'next-translate/useTranslation';
+
+import Contacts from '../contacts/contacts';
+import PopupButtons from './popupButtons';
+import AddContactsPopup from '../popups/addContactPopup/addContactsPopup';
+import RequestsPopup from '../popups/requestsPopup/requestsPopup';
+
+import { ContactsContext } from '../../providers/contactsProvider/contactsProvider';
+
+import classes from './sidebar.module.scss';
+
+import MobileMenu from '../mobileMenu/mobileMenu';
+
+function Sidebar() {
+  const router = useRouter();
+  const [ addContactPopup, setAddContactPopup ] = useState(false);
+  const [ showRequestsPopup, setShowRequestsPopup ] = useState(false);
+  const { activeContact } = useContext(ContactsContext)
   const { t } = useTranslation('sidebar');
+
   return (
-    <div className={ classes.sidebar }>
+    <div className={[ classes.sidebar, activeContact && classes.hidden ].join(' ')}>
+      <AddContactsPopup   
+        displayState={ addContactPopup } 
+        setDisplayState={ setAddContactPopup } 
+      />
+      <RequestsPopup
+        displayState={ showRequestsPopup } 
+        setDisplayState={ setShowRequestsPopup } 
+      />
       <div className={ classes.header }>
         <h1>
           { t('title') }
         </h1>
       </div>
       <div className={ classes.contacts }>
-        <PopupButtons 
+        <PopupButtons
           setAddContactPopup={ setAddContactPopup }
           setShowRequestsPopup={ setShowRequestsPopup }
         />
         <Contacts />
       </div>
       <div className={ classes.footer }>
-        <button className={ classes.buttonBlockDark2 }>
+        <button onClick={ _ => router.push('panel/settings')} className={ classes.buttonBlockDark2 }>
           { t('settings') }
         </button>
-        <button className={ classes.buttonBlockDark2 }>
-          { t('about') }
-        </button>
       </div>
+      <MobileMenu
+        setAddContactPopup={ _ => setAddContactPopup(true) }
+        setShowRequestsPopup={ _ => setShowRequestsPopup(true) }
+      />
     </div>
   )
 }
