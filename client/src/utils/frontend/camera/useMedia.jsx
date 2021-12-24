cfcc53d3dc012c5eab37d3aad4a56cbf45dc2ec0
Amir Arab <amir4rab@gmail.com>
2021-12-24 17:48:26 +0330

    useMedia bug fixes

--- client/src/utils/frontend/camera/useMedia.jsx
+++ client/src/utils/frontend/camera/useMedia.jsx
@@ -1,84 +1,51 @@
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect, useCallback } from 'react';
 
-const defaultMediaOptions = {
-  audio: true,
-  video: {
-    height: 360,
-    width: 640,
-    facingMode: "user",
-    aspectRatio: ""
-  }
-}
 
-export const useMedia = ( videoRef,  mediaOptions = defaultMediaOptions ) => {
+export const useMedia = ( videoRef, audioOnly = false ) => {
   const [ mediaIsGranted, setMediaIsGranted ] = useState(false);
-  const [ cameraSettings, setCameraSettings ] = useState(mediaOptions.video);
-  const [ audioSettings, setAudioSettings ] = useState(mediaOptions.audio);
   const [ mediaSettings, setMediaSettings ] = useState({
-    video: mediaOptions.video,
-    audio: mediaOptions.audio
+    video: audioOnly ? false : {
+      height: 360,
+      width: 640,
+      facingMode: "user",
+      aspectRatio: ""
+    },
+    audio: true
   })
-  const [ cachedCameraSettings, setCachedCameraSettings ] = useState(null);
   const mediaStreamRef = useRef(null);
-  const [ stream, setStream ] = useState(null)
-  // const [ mediaStream, setMediaStream ] = useState(null);
 
-  const getMedia = async ( settings = mediaSettings ) => {
+  const getMedia = useCallback(async () => {
     try {
-      mediaStreamRef.current?.getTracks()?.forEach( track => {
-        // console.log('track stopped!')
-        track.stop();
-      });
-      if( !cameraSettings && !audioSettings ) {
-        videoRef.current.srcObject = null;
-        mediaStreamRef.current = null;
-        return;
-      } else {
-        console.log(cameraSettings, audioSettings)
-        const mediaStream = await navigator.mediaDevices.getUserMedia(settings);
-        videoRef.current.srcObject = mediaStream;
-        mediaStreamRef.current = mediaStream;
-        setMediaIsGranted(true);
-        setStream(mediaStream);
-        // console.log('MediaStream Started!');
-      }
+      const mediaStream = await navigator.mediaDevices.getUserMedia(mediaSettings);
+      videoRef.current.srcObject = mediaStream;
+      mediaStreamRef.current = mediaStream;
+      setMediaIsGranted(true);
     } catch {
       console.error('Some thing went wrong when accessing camera!')
     }
+  }, [ mediaSettings, videoRef ])
+
+  const closeMedia = () => {
+    mediaStreamRef.current?.getTracks()?.forEach( track => {
+      track.stop();
+    });
   }
 
   useEffect( _ => {
-    getMedia();
+    if( mediaStreamRef.current === null )  getMedia();
     return () => {
-      mediaStreamRef.current?.getTracks()?.forEach( track => {
-        track.stop();
-      });
+      closeMedia();
     }
-  }, []);
-
-  // useEffect( _ => {
-  //   // console.log('here!');
-  //   // getMedia({
-  //   //   video: cameraSettings,
-  //   //   audio: audioSettings
-  //   // });
-  //   setMediaSettings({
-  //     video: cameraSettings,
-  //     audio: audioSettings
-  //   })
-  // }, [ cameraSettings, audioSettings ])
+  }, [ getMedia ]);
 
   const changeMedia = async ( changedItem, newValue ) => {
     switch( changedItem ) {
       case 'audio': {
-        console.log('new audio value: ', newValue)
         mediaStreamRef.current.getTracks().forEach(track => {
           if( track.kind === 'audio' ) {
             track.enabled = newValue
           }
         });
-
-        setAudioSettings(newValue);
         break;
       }
       case 'camera': {
@@ -95,7 +62,8 @@ export const useMedia = ( videoRef,  mediaOptions = defaultMediaOptions ) => {
   return ([
     changeMedia,
     mediaStreamRef,
-    mediaIsGranted
+    mediaIsGranted,
+    closeMedia
   ])
 };
 
