269330670247144a435acc14fea2e213d77a7e29
Amir Arab <amir4rab@gmail.com>
2021-12-26 16:54:17 +0330

    waiting display added

--- TODO.md
+++ TODO.md
@@ -1,17 +1,18 @@
 # To do
 
 ## New features
-- [ ] Fix notifications
 - [ ] Add camera rotation
 - [ ] Add delete account
 - [ ] Add e2ee chat
 - [ ] Finish settings
-- [ ] Complete translation of call displays
+- [x] Complete translation of call displays
 - [ ] About page
 - [ ] Banning users
 - [ ] Add Translation and Call time to Ended call display
 - [ ] Add more options to call display
+- [ ] adding loading screen
 
 ## Bug fixes
 - [ ] Call ended before answer bug fix
 - [ ] Fix waiting display bug
+- [ ] Fix notifications
---
+++ client/src/components/callDisplays/waitingDisplay/waitingDisplay.jsx
@@ -0,0 +1,60 @@
+import { useState, useRef, useEffect, useContext } from 'react'
+
+import { IoVolumeMute, IoVolumeMedium } from 'react-icons/io5';
+
+import classes from './waitingDisplay.module.scss';
+
+import useTranslation from 'next-translate/useTranslation';
+import waitingAudio from './audio.json';
+import { ContactsContext } from '../../../providers/contactsProvider/contactsProvider';
+
+function WaitingDisplay({ callTo, mutedByDefault = false, isCalling }) {
+  const { getContact } = useContext(ContactsContext);
+  const [ isMuted, setIsMuted ] = useState(false);
+  const audioRef = useRef();
+  const { t } = useTranslation('waitingDisplay');
+  const { t: tCallDisplay } = useTranslation('callDisplay');
+
+
+  useEffect( _ => {
+    if ( !mutedByDefault ) {
+      audioRef.current.volume = 0.1;
+      audioRef.current.play();
+    }
+  }, [ audioRef, mutedByDefault ]);
+
+  return (
+    <div className={ classes.waitingDisplay }>
+      <div className={ classes.nameWrapper }>
+        <div className={ classes.name }>
+          { getContact(callTo).name.slice(0,1) }
+        </div>
+      </div>
+      <div className={ classes.text }>
+        { `${ isCalling ? t('calling') : t('connecting') } ${getContact(callTo).name}` }
+      </div>
+      <div className={ classes.buttonWrapper }>
+        {
+          !mutedByDefault ?
+          <button onClick={ _ => setIsMuted(!isMuted) }>
+            {
+              !isMuted ?
+              <>
+                <IoVolumeMute />
+                <p>{tCallDisplay('audioOn')}</p>
+              </>
+              :
+              <>
+                <IoVolumeMedium />
+                <p>{tCallDisplay('audioOff')}</p>
+              </>
+            }
+          </button> : null
+        }
+      </div>
+      <audio muted={ isMuted } ref={ audioRef } loop src={ waitingAudio.audio } />
+    </div>
+  )
+}
+
+export default WaitingDisplay
