9979298eafc88ef5550fe97b3ceff1375b9a42a2
Amir Arab <amir4rab@gmail.com>
2022-01-15 17:41:15 +0330

    bug fixes

--- client/pages/_document.jsx
+++ client/pages/_document.jsx
@@ -10,6 +10,14 @@ class MyDocument extends Document {
     return (
       <Html lang='en'>
         <Head>
+          <link 
+            href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap" 
+            rel="stylesheet"
+          />
+          <link 
+            href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" 
+            rel="stylesheet"
+          /> 
           <meta name="description" content="Secure e2ee video and audio calls" />
           <link rel="icon" href="/favicon.svg" />
         </Head>
