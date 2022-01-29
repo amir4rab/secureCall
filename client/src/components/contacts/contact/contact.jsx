a9b01e50915e33c5e8a09134d421e2ef50cabb88
Amir Arab <amir4rab@gmail.com>
2022-01-29 09:51:27 +0330

    modified contact component

--- client/src/components/contacts/contact/contact.jsx
+++ client/src/components/contacts/contact/contact.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import classes from './contact.module.scss';
 
-function Contact({ contact, onClick }) {
+function Contact({ contact, onClick, customContact= false, children }) {
+
+  if( customContact ) return (
+    <div onClick={ onClick } className={ classes.contact }>
+      { children }
+    </div>
+  )
+
   return (
     <div onClick={ onClick } className={ classes.contact }>
       <div className={ classes.imageWrapper }>
-        {
-          // contact.image !== null ? 
-          // <img className={ classes.image } src={ contact.image } alt={ `profile image of ${contact.name}` } />
-          // :
-          <p className={ classes.imagePlaceHolder }>
-            { contact.name.slice(0,1) }
-          </p>
-        }
+        <p className={ classes.imagePlaceHolder }>
+          { contact.name.slice(0,1) }
+        </p>
       </div>
       <div className={ classes.nameWrapper }>
         { contact.name }
