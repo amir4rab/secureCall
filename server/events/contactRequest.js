67d731d24b2341fe25ce1429c4ac02961d5b6f0c
Amir Arab <amir4rab@gmail.com>
2022-01-30 10:20:35 +0330

    rewritten contactRequest

--- server/events/contactRequest.js
+++ server/events/contactRequest.js
@@ -6,137 +6,155 @@ const contactRequest = async ( data, callback, { client, activeUsers, socket } )
   const {
     recipientEmail
   } = data;
-  await client.connect();
-  const database = client.db('secureCall');
-  const users = database.collection('users');
-  const query = {
-    email: recipientEmail
-  };
 
-  console.log(socket.id);
-  const userDbData = await users.findOne({ email: socketEmail });
-
-  const recipientEmailIsValid = validator.isEmail(recipientEmail);
-  if ( !recipientEmailIsValid ) {
+  const recipientEmailIsValid = validator.isEmail(recipientEmail); 
+  if ( !recipientEmailIsValid ) { //** verifying recipient email address **//
     await client.close();
-    try {
-      callback({
+    callback && callback({
         recipientEmail,
         status: 'error',
         response: 'Invalid Email address!',
         responseCode: 'invalidInput'
       });
-    } catch {};
     return;
   }
 
-  const receiver = await users.findOne(query);
+  await client.connect();
+  const database = client.db('secureCall');
+  const users = database.collection('users');
+
+  const userDbData = await users.findOne({ email: socketEmail });
+  const receiver = await users.findOne({ email: recipientEmail });
 
   if( receiver === null ) { //** checks if the recipient does not exists **//
     await client.close();
-    try {
-      callback({
+    callback && callback({
         recipientEmail,
         status: 'error',
         response: 'User does not exists!',
         responseCode: 'userDNE'
       });
-    } catch {};
     return;
-  } else if ( recipientEmail === userDbData.email ) { //** checks if the recipient email is wrong **//
+  };
+
+  if ( recipientEmail === userDbData.email ) { //** checks if the recipient email is wrong **//
     await client.close();
-    try {
-      callback({
+    callback && callback({
+      recipientEmail,
+      status: 'error',
+      response: "You can't sent a request to yourself!",
+      responseCode: 'selfReq'
+    });
+    return;
+  };
+
+  if ( receiver.banList.includes( socketEmail ) ) { //** recipient have blocked senders account **//
+    await client.close();
+    callback && callback({
         recipientEmail,
         status: 'error',
-        response: "You can't sent a request to yourself!",
-        responseCode: 'selfReq'
+        response: `${recipientEmail} inbox is full!`,
+        responseCode: 'blockedYou'
       });
-    } catch {};
     return;
-  }
+  };
+
+  if ( userDbData.banList.includes( recipientEmail ) ) { //** senders have blocked recipient account **//
+    await client.close();
+    callback && callback({
+      recipientEmail,
+      status: 'error',
+      response: `${recipientEmail} inbox is full!`,
+      responseCode: 'youBlocked'
+    });
+    return;
+  };
 
   if ( receiver.requests.length > 10 ) { //** recipient request inbox is full **//
 
     await client.close();
-    try {
-      callback({
-        recipientEmail,
-        status: 'error',
-        response: `${recipientEmail} inbox is full!`,
-        responseCode: 'fullInbox'
-      });
-    } catch {};
-  } else if ( typeof receiver.requests.find( request => request.email === userDbData.email ) !== 'undefined' ) { //** recipient already has a request from sender **//
+    callback && callback({
+      recipientEmail,
+      status: 'error',
+      response: `${recipientEmail} inbox is full!`,
+      responseCode: 'fullInbox'
+    });
+    return;
+  };
+
+  const alreadyInRequests = receiver.requests.find( request => request.email === userDbData.email );
+  if ( typeof alreadyInRequests !== 'undefined' ) { //** recipient already has a request from sender **//
 
     await client.close();
-    try {
-      callback({
-        recipientEmail,
-        status: 'successful',
-        response: `${recipientEmail} already has a request from you!`,
-        responseCode: 'dupReq'
-      });
-    } catch {};
+    callback && callback({
+      recipientEmail,
+      status: 'successful',
+      response: `${recipientEmail} already has a request from you!`,
+      responseCode: 'dupReq'
+    });
+    return;
 
-  } else if ( typeof receiver.contacts.find( contact => contact.email === userDbData.email ) !== 'undefined' ) { //** recipient is already a contact **//
+  };
 
-    await client.close();
-
-    try {
-      callback({
-        recipientEmail,
-        status: 'successful',
-        response: `${recipientEmail} is in your contacts!`,
-        responseCode: 'dupCon'
-      });
-    } catch {};
-
-  } else if ( typeof userDbData.requests.find( request => request.email === receiver.email ) !== 'undefined' ) { //** request sender already has a request from the receiver **//
+  const alreadyInContacts = receiver.contacts.find( contact => contact.email === userDbData.email );
+  if ( typeof alreadyInContacts !== 'undefined' ) { //** recipient is already a contact **//
 
     await client.close();
-    try {
-      callback({
-        recipientEmail,
-        status: 'error',
-        response: `You already have a request from ${recipientEmail}!`,
-        responseCode: 'hasReq'
-      })
-    } catch {};
-  } else { //** adding request to recipient's request inbox  **//
-    const requests = [
-      {
-        email: userDbData.email,
-        name: userDbData.name
-      },
-      ...receiver.requests
-    ];
-  
-    const updateDocument = {
-      $set: {
-        requests,
-      },
-    };
-  
-    await users.updateOne({ _id: receiver._id }, updateDocument );
-    await client.close();
-    const recipientSocketId = getIdFromEmail(recipientEmail, activeUsers);
-    if ( recipientSocketId !== null ) { //** sending recipient the request event **//
-      socket.to(recipientSocketId).emit('contactingRequest', ({
-        name: userDbData.name,
-        email: userDbData.email
-      }));
-    }
 
-    try {
-      callback({
-        recipientEmail,
-        status: 'successful',
-        response: 'Request has been sent.',
-        responseCode: 'successful'
-      });
-    } catch {};
+    callback && callback({
+      recipientEmail,
+      status: 'successful',
+      response: `${recipientEmail} is in your contacts!`,
+      responseCode: 'dupCon'
+    });
+    return;
+
+  };
+
+  const senderHaveRequest = userDbData.requests.find( request => request.email === receiver.email );
+  if ( typeof senderHaveRequest !== 'undefined' ) { //** request sender already has a request from the receiver **//
+
+    await client.close();
+    callback && callback({
+      recipientEmail,
+      status: 'error',
+      response: `You already have a request from ${recipientEmail}!`,
+      responseCode: 'hasReq'
+    });
+    return;
   }
+
+  //** adding request to recipient's request inbox  **//
+  const requests = [
+    {
+      email: userDbData.email,
+      name: userDbData.name
+    },
+    ...receiver.requests
+  ];
+
+  const updateDocument = {
+    $set: {
+      requests,
+    },
+  };
+
+  await users.updateOne({ _id: receiver._id }, updateDocument );
+  await client.close();
+  const recipientSocketId = getIdFromEmail(recipientEmail, activeUsers);
+  if ( recipientSocketId !== null ) { //** sending recipient the request event **//
+    socket.to(recipientSocketId).emit('contactingRequest', ({
+      name: userDbData.name,
+      email: userDbData.email
+    }));
+  }
+
+  callback && callback({
+    recipientEmail,
+    status: 'successful',
+    response: 'Request has been sent.',
+    responseCode: 'successful'
+  });
 }
 
 module.exports = contactRequest;
