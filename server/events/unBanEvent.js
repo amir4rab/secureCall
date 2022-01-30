d1bc0996a1f3c069482e999791560e4ec075a4a9
Amir Arab <amir4rab@gmail.com>
2022-01-30 10:21:14 +0330

    added unBlockEvent

---
+++ server/events/unBanEvent.js
@@ -0,0 +1,39 @@
+const getIdFromEmail = require('../utils/getIdFromEmail');
+const isEmailValid = require('../utils/isEmailValid');
+
+const ubBanEvent = async ( data, callback, { client, activeUsers, socket } ) => {
+  const socketEmail = activeUsers.get(socket.id);
+  const {
+    email: recipientEmail
+  } = data;
+
+  if ( !isEmailValid( recipientEmail ) ){
+    callback && callback({
+      status: 'error',
+      response: 'Invalid inputs!',
+      responseCode: 'invalid'
+    })
+    return;
+  }
+
+  await client.connect();
+  const database = client.db('secureCall');
+  const users = database.collection('users');
+  const userDbData = await users.findOne({ email: socketEmail });
+  const updatedUserDocument = {
+    $set: {
+      banList: userDbData.banList.filter( contact => contact.email !== recipientEmail )
+    },
+  };
+
+  await users.updateOne({ _id: userDbData._id }, updatedUserDocument );
+
+  await client.close();
+
+  callback && callback({
+    status: 'successful',
+    response: 'Successfully accepted the request.'
+  });
+};
+
+module.exports = ubBanEvent;
