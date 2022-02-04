662cf9fbf837f0100afab0052a5ff77839f36ae7
Amir Arab <amir4rab@gmail.com>
2022-02-04 11:13:56 +0330

    added help to settings

--- client/pages/panel/settings.jsx
+++ client/pages/panel/settings.jsx
@@ -2,7 +2,11 @@ import Head from 'next/head';
 import { motion } from 'framer-motion';
 import Settings from '../../src/components/settings/settings';
 
-function SettingsPage() {
+import getMarkdown from '../../src/utils/backend/getMarkdown/getMarkdown';
+import getJson from '../../src/utils/backend/getJson/getJson';
+import jsonToTable from '../../src/utils/backend/jsonToTable/jsonToTable';
+
+function SettingsPage({ data }) {
   return (
     <>
       <Head>
@@ -13,10 +17,49 @@ function SettingsPage() {
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
       >
-        <Settings />
+        <Settings data={ data } />
       </motion.div>
     </>
   )
 }
+
+export async function getStaticProps() {
+  const troubleshooting = await getMarkdown('troubleshooting');
+
+  const testedBrowsersRaw = await getJson('testedBrowsers');
+  const testedBrowsers = jsonToTable(testedBrowsersRaw);
+  
+  const browserSupportRaw = await getJson('browserSupport');
+  const browserSupport = jsonToTable(browserSupportRaw);
+
+  const map = new Map([
+    ['browserSupport', 'Browser support'],
+    ['testedBrowsers', 'Tested browsers'],
+    ['troubleshooting', 'troubleshooting'],
+  ]);
+  
+  const mapFileNamesToDisplayNames = ( dataArr ) => {
+    const newDataArr = dataArr.map( element => ({
+      ...element,
+      displayName: map.get(element.name),
+    }))
+    return newDataArr;
+  };
+
+  const helpDataArr = mapFileNamesToDisplayNames([
+    browserSupport,
+    testedBrowsers,
+    troubleshooting,
+  ]);
+  
+  return {
+    props: {
+      data: {
+        helpData: helpDataArr
+      }
+    }
+  }
+}
+
 
 export default SettingsPage
--- client/src/components/settings/helpSection/helpSection.jsx
+++ client/src/components/settings/helpSection/helpSection.jsx
@@ -1,21 +1,26 @@
-import React from 'react'
+import { motion } from 'framer-motion';
 
 import classes from './helpSection.module.scss';
 
 import { IoChevronBack } from 'react-icons/io5';
 import useTranslation from 'next-translate/useTranslation';
 
+import { MotionWrapper, parentVariants } from '../settingsAnimations';
 
-function HelpSection({ close }) {
+function HelpSection({ close, data }) {
   const { t } = useTranslation('settings');
-
   return (
-    <div className={ classes.helpSection }>
+    <motion.div variants={ parentVariants } animate='visible' initial='hidden' className={ classes.helpSection }>
       <div className={ classes.titleBox }>
         <button onClick={ close } className={ classes.backButton }><IoChevronBack /></button>
         <h3 className={ classes.title }>{t('help')}</h3>
       </div>
-    </div>
+      <main className={ classes.markdownChildren }>
+        {
+          data.map(item => <MotionWrapper style={{ paddingBottom: '2.5rem' }} key={ item.name }><article dangerouslySetInnerHTML={{ __html: item.html }} /></MotionWrapper>)
+        }
+      </main>
+    </motion.div>
   );
 };
 
