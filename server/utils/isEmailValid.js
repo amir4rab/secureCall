ea3173af54c62e814bf52af6d926a4c2036b7171
Amir Arab <amir4rab@gmail.com>
2022-02-09 10:56:01 +0330

    added safety checks to email validator

--- server/utils/isEmailValid.js
+++ server/utils/isEmailValid.js
@@ -1,22 +1,25 @@
 const validator = require('validator');
 
 const isEmailValid = async ( recipientEmail, callback = null ) => {
-  const recipientEmailIsValid = validator.isEmail(recipientEmail);
-  if ( !recipientEmailIsValid ) {
-    await client.close();
-    try {
-      if ( callback !== null ) {
-        callback({
+  try {
+    if( typeof recipientEmail !== 'string' ) return false;
+    console.log(recipientEmail);
+    const recipientEmailIsValid = validator.isEmail(recipientEmail);
+    if ( !recipientEmailIsValid ) {
+      await client.close()
+      callback && callback({
           recipientEmail,
           status: 'error',
           response: 'Invalid Email address!',
           responseCode: 'invalidInput'
-        });
-      }
-    } catch {};
+      });
+      return false;
+    }
+    return true;
+  } catch(err) {
+    console.error(err);
     return false;
   }
-  return true;
 }
 
 module.exports = isEmailValid;
